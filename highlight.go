0e05272341eaf669e0282d7733f1d5e1ced6aba6
maxlandon <maximelandon@gmail.com>
2022-12-24 21:31:05 +0100

    Enable syntax highlight combined with visual

--- highlight.go
+++ highlight.go
@@ -1,25 +1,26 @@
 package readline
 
 import (
+	"regexp"
 	"sort"
 )
 
 // region is a part of the input line for which to apply some highlighting.
 type region struct {
-	regionType  string
-	bpos        int
-	epos        int
-	highlightFg string
-	highlightBg string
+	regionType string
+	bpos       int
+	epos       int
+	fg         string
+	bg         string
 }
 
 func (rl *Instance) addRegion(rtype string, bpos, epos int, fg, bg string) {
 	hl := region{
-		regionType:  rtype,
-		bpos:        bpos,
-		epos:        epos,
-		highlightFg: fg,
-		highlightBg: bg,
+		regionType: rtype,
+		bpos:       bpos,
+		epos:       epos,
+		fg:         fg,
+		bg:         bg,
 	}
 
 	rl.regions = append(rl.regions, hl)
@@ -46,13 +47,40 @@ func (rl *Instance) getHighlights(line []rune) map[int][]rune {
 		}
 	}
 
+	// Find any highlighting already applied on the line,
+	// and keep the indexes so that we can skip those.
+	var colors [][]int
+
+	colorMatch, err := regexp.Compile(`\x1b\[[0-9;]+m`)
+	if err == nil {
+		colors = colorMatch.FindAllStringIndex(string(line), -1)
+	}
+
 	// regions that started highlighting, but not done yet.
 	pending := make([]*region, 0)
+	lineIndex := -1
+	skip := 0
 
 	// Build the string.
-	for lineIndex := range line {
+	for rawIndex := range line {
 		var posHl []rune
 		var newHl *region
+
+		// While in a color escape, keep reading runes.
+		if skip > 0 {
+			skip--
+			continue
+		}
+
+		// If starting a color escape code, add offset and read.
+		if len(colors) > 0 && colors[0][0] == rawIndex {
+			skip += colors[0][1] - colors[0][0] - 1
+			colors = colors[1:]
+			continue
+		}
+
+		// Or we are reading a printed rune.
+		lineIndex += 1
 
 		// First check if we have a new highlighter to apply
 		for _, reg := range sorted {
@@ -75,17 +103,18 @@ func (rl *Instance) getHighlights(line []rune) map[int][]rune {
 
 		// If we have a new higlighting, apply it.
 		if newHl != nil {
-			posHl = append(posHl, []rune(newHl.highlightBg)...)
-			posHl = append(posHl, []rune(newHl.highlightFg)...)
+			posHl = append(posHl, []rune(newHl.bg)...)
+			posHl = append(posHl, []rune(newHl.fg)...)
 		} else if len(pending) > 0 && doneReset {
 			backHl := pending[len(pending)-1]
-			posHl = append(posHl, []rune(backHl.highlightBg)...)
-			posHl = append(posHl, []rune(backHl.highlightFg)...)
+			posHl = append(posHl, []rune(backHl.bg)...)
+			posHl = append(posHl, []rune(backHl.fg)...)
 		}
 
-		// Add to the line.
+		// Add to the line, with the raw index since
+		// we must take into account embedded colors.
 		if len(posHl) > 0 {
-			hl[lineIndex] = posHl
+			hl[rawIndex] = posHl
 		}
 	}
 
