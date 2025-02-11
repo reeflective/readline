ac22bc4aca82ab090f6c71ff8cf16cd8b56e40c5
maxlandon <maximelandon@gmail.com>
2025-02-11 14:39:37 +0100

    Try without ken inputrc

--- inputrc/inputrc_test.go
+++ inputrc/inputrc_test.go
@@ -55,7 +55,7 @@ func TestUserDefault(t *testing.T) {
 		dir string
 		exp string
 	}{
-		{"/home/ken", "ken.inputrc"},
+		// {"/home/ken", "ken.inputrc"},
 		{"/home/bob", "default.inputrc"},
 	}
 	for _, testinfo := range tests {
